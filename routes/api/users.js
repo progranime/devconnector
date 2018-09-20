3d4483183b2e9fffdfe44c7d014b7c022d856d8e
progranime <jeremyespinosa1995@gmail.com>
2018-09-20 15:48:40 +0800

    added models

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,77 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs') // encryption
+// Load User Model
+const User = require('../../models/user')
 
 // @router  GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Users Works"}))
+
+// @router  GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+    // find if the email exist
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ email: 'Email already exists' })
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // size
+                    r: 'pg', // Rating
+                    d: 'mm' // Default
+                })
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        // if (err) throw err
+
+                        newUser.password = hash
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
+                })
+            }
+        })
+})
+
+// @router  GET api/users/login
+// @desc    Login user / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    // Find user by email
+    User.findOne({ email })
+        .then(user => {
+            // Check for User
+            if (!user)  return res.status(404).json({ email: 'User not found' })
+
+            // Check Password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success'})
+                    } else {
+                        return res.status(400).json({ password: 'Password incorrect'})
+                    }
+                })
+        })
+})
+
 
 module.exports = router
